183093c5f30ffc6f7a9d84efafe20b5c6f11be36
Sarah Majors <sfmajors373@gmail.com>
2022-10-09 21:58:01 +0000

    Update

---
+++ data_sources.py
@@ -0,0 +1,13 @@
+DATA_SOURCES = {
+    'mariupol': {
+        # mariupol is a mosaic of two images
+        # the left image ends in TCN
+        # the right image ends in TDN
+        'annotations': [
+        ],
+        'rasters': [
+        ],
+        'no_analysis': [
+        ],
+    }
+}
